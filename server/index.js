bfe43b85c8c051d3f889d1db13fb4427381377bf
Moad <moaddona@gmail.com>
2026-03-20 19:19:49 +0100

    fix: enable trust proxy for Railway deploymenttry 02

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-app.set('trust proxy', 1);
 const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
@@ -7,8 +6,8 @@ require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
-
 const app = express();
+app.set('trust proxy', 1);
 
 app.use(helmet());
 
